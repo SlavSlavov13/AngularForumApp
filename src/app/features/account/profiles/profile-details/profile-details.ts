3dbc57093f842a83f8ca93b638b7c359cf87a81a
Slav Slavov <slavslavov10@yahoo.com>
2025-08-18 20:50:28 +0300

    migrate profile components to NgRx-based loading state: replace local loading properties with store-driven selectors, update templates for state-managed loaders, adjust `ProfileDetails` logic for dispatching loading actions, and align conditional UI rendering with observable state.

--- src/app/features/account/profiles/profile-details/profile-details.ts
+++ src/app/features/account/profiles/profile-details/profile-details.ts
@@ -4,30 +4,40 @@ import {AuthService} from "../../../../core/services/auth.service";
 import {AppUserModel} from "../../../../shared/models";
 import {ActivatedRoute} from "@angular/router";
 import {handleError} from "../../../../shared/helpers";
+import {Store} from "@ngrx/store";
+import {Observable} from "rxjs";
+import {AppState, hideLoading, selectLoadingVisible, showLoading} from "../../../../store";
+import {AsyncPipe} from "@angular/common";
 
 @Component({
 	selector: 'app-profile-details',
-	imports: [ProfileCard],
+	imports: [ProfileCard, AsyncPipe],
 	templateUrl: './profile-details.html',
 	styleUrl: './profile-details.css'
 })
 export class ProfileDetails implements OnInit {
-	loading: boolean = true;
+	loading$: Observable<boolean> = this.store.select(selectLoadingVisible);
 	error: string | null = null;
 	user!: AppUserModel;
+	userLoaded: boolean = false;
 
-	constructor(private route: ActivatedRoute, private authService: AuthService) {
+	constructor(
+		private route: ActivatedRoute,
+		private authService: AuthService,
+		private store: Store<AppState>,
+	) {
 	}
 
 	async ngOnInit(): Promise<void> {
 		try {
+			this.store.dispatch(showLoading());
 			const uid: string = this.route.snapshot.paramMap.get('id')!;
 			this.user = await this.authService.getUser(uid);
+			this.userLoaded = true;
 		} catch (e) {
 			this.error = handleError(e);
 		} finally {
-			this.loading = false;
+			this.store.dispatch(hideLoading());
 		}
 	}
-
 }
