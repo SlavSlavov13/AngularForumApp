89218ae71d59efee5256b40911506bdfcd037914
Slav Slavov <slavslavov10@yahoo.com>
2025-08-19 17:14:04 +0300

    integrate post-delete component: replace inline delete button in posts-visualization with reusable post-delete component, add event emitters for deletion state management, and refactor templates and logic for streamlined post removal handling.

--- src/app/features/posts/post-delete/post-delete.ts
+++ src/app/features/posts/post-delete/post-delete.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {PostService} from "../../../core/services/post.service";
+import {PostModel} from "../../../shared/models";
+import {firstValueFrom} from "rxjs";
+import {handleError} from "../../../shared/helpers";
 
 @Component({
-  selector: 'app-post-delete',
-  imports: [],
-  templateUrl: './post-delete.html',
-  styleUrl: './post-delete.css'
+	selector: 'app-post-delete',
+	imports: [],
+	templateUrl: './post-delete.html',
+	styleUrl: './post-delete.css'
 })
 export class PostDelete {
+	@Input({required: true}) postId!: string;
+	@Output() deleting: EventEmitter<void> = new EventEmitter<void>;
+	@Output() postDeleted: EventEmitter<string> = new EventEmitter<string>();
+	error?: string;
 
+	constructor(
+		private postService: PostService
+	) {
+	}
+
+
+	async delete(): Promise<void> {
+		try {
+			this.deleting.emit();
+			const post: PostModel = (await firstValueFrom(this.postService.getPost(this.postId)))!;
+			const ok: boolean = confirm(`Delete post "${post.body}"? This cannot be undone.`);
+			if (!ok) return;
+			await firstValueFrom(this.postService.deletePost(this.postId));
+			this.postDeleted.emit(this.postId);
+		} catch (e) {
+			this.error = handleError(e);
+		} finally {
+		}
+	}
 }
--- src/app/features/posts/posts-visualization/posts-visualization.ts
+++ src/app/features/posts/posts-visualization/posts-visualization.ts
@@ -5,13 +5,15 @@ import {Observable} from "rxjs";
 import {AppState, selectLoadingVisible} from "../../../store";
 import {Store} from "@ngrx/store";
 import {RouterLink} from "@angular/router";
+import {PostDelete} from "../post-delete/post-delete";
 
 @Component({
 	selector: 'app-posts-visualization',
 	imports: [
 		AsyncPipe,
 		DatePipe,
-		RouterLink
+		RouterLink,
+		PostDelete
 	],
 	templateUrl: './posts-visualization.html',
 	styleUrl: './posts-visualization.css'
@@ -24,9 +26,18 @@ export class PostsVisualization {
 	@Input() postInThread?: boolean;
 	@Input() profileCardUser?: AppUserModel;
 	loading$: Observable<boolean> = this.store.select(selectLoadingVisible);
+	deleting: boolean = false;
 
 	constructor(
 		private store: Store<AppState>,
 	) {
+	}
+
+	onDeleting(): void {
+		this.deleting = true;
+	}
+
+	onPostDeleted(deletedPostId: string): void {
+		this.posts = this.posts.filter(post => post.id !== deletedPostId);
 	}
 }
