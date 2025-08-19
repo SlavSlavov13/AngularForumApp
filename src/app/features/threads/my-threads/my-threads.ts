8f8d1223fc6faf05cab32644574fe15f4b50bcb5
Slav Slavov <slavslavov10@yahoo.com>
2025-08-19 12:38:33 +0300

    migrate my-threads component to NgRx-based loading management: replace local loading state with store-driven observable, update template for conditional UI rendering, dispatch loading actions during initialization, and align logic with state management practices.

--- src/app/features/threads/my-threads/my-threads.ts
+++ src/app/features/threads/my-threads/my-threads.ts
@@ -3,6 +3,9 @@ import {ProfileThreadsList} from '../profile-threads-list/profile-threads-list';
 import {AuthService} from '../../../core/services/auth.service';
 import {CommonModule} from "@angular/common";
 import {handleError} from "../../../shared/helpers";
+import {Observable} from "rxjs";
+import {AppState, hideLoading, selectLoadingVisible, showLoading} from "../../../store";
+import {Store} from "@ngrx/store";
 
 
 @Component({
@@ -14,22 +17,25 @@ import {handleError} from "../../../shared/helpers";
 })
 export class MyThreads implements OnInit {
 	uid!: string;
-	loading: boolean = true;
 	error: string | null = null;
+	loading$: Observable<boolean> = this.store.select(selectLoadingVisible);
 
-
-	constructor(protected authService: AuthService) {
+	constructor(
+		protected authService: AuthService,
+		private store: Store<AppState>,
+	) {
 	}
 
 	@Input() profileCard: boolean = false;
 
 	async ngOnInit(): Promise<void> {
 		try {
+			this.store.dispatch(showLoading());
 			this.uid = (await this.authService.currentUid())!;
 		} catch (e) {
 			this.error = handleError(e);
 		} finally {
-			this.loading = false;
+			this.store.dispatch(hideLoading());
 		}
 	}
 }
